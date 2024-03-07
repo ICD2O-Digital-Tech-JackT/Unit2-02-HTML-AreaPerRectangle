a501d39a427de117438b25cd7ca49d3a59b753f3
Jack Turcotte <159164296+JackTurcs@users.noreply.github.com>
2024-03-07 16:04:35 +0000

    finished

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+function DisplayArea(x){
+  document.getElementById("Area").innerHTML=x
+}
+
+
+function CalculateArea(Length,Width){
+  DisplayArea(Length*Width)
+}
+
+function DisplayPerimeter(x){
+  document.getElementById("Perimeter").innerHTML=x
+}
+
+
+function CalculatePerimeter(Length,Width){
+  DisplayPerimeter((2*Length)+(2*Width))
+}
